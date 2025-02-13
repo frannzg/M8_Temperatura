5c8551ae46c335b52354f4232a8597cf311f55ea
frannzg <148803161+frannzg@users.noreply.github.com>
2025-02-13 18:47:33 +0100

    Initial commit

---
+++ app/src/main/java/com/frannzg/temperatura/Temperatura.java
@@ -0,0 +1,60 @@
+package com.frannzg.temperatura;
+
+public class Temperatura {
+    private String cel;
+    private String data;
+    private int humitat;
+    private double temperatura;
+
+    public Temperatura() {
+    }
+
+    public Temperatura(String cel, String data, int humitat, double temperatura) {
+        this.cel = cel;
+        this.data = data;
+        this.humitat = humitat;
+        this.temperatura = temperatura;
+    }
+
+    public String getCel() {
+        return cel;
+    }
+
+    public void setCel(String cel) {
+        this.cel = cel;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getHumitat() {
+        return humitat;
+    }
+
+    public void setHumitat(int humitat) {
+        this.humitat = humitat;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public String toString() {
+        return "Temperatura{" +
+                "cel='" + cel + '\'' +
+                ", data='" + data + '\'' +
+                ", humitat=" + humitat +
+                ", temperatura=" + temperatura +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/frannzg/temperatura/ui/home/HomeFragment.java
@@ -0,0 +1,63 @@
+package com.frannzg.temperatura.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.frannzg.temperatura.Temperatura;
+import com.frannzg.temperatura.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class HomeFragment extends Fragment implements View.OnClickListener{
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+
+        binding.btSave.setOnClickListener(this);
+
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == binding.btSave.getId()){
+
+            Temperatura temperatura = new Temperatura();
+            temperatura.setCel(binding.etCel.getText().toString());
+            temperatura.setData(binding.etData.getText().toString());
+            temperatura.setHumitat(Integer.parseInt(binding.etHumitat.getText().toString()));
+            temperatura.setTemperatura(Double.parseDouble(binding.etTemperatura.getText().toString()));
+
+
+
+            // Write a message to the database
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("temperatura");
+            myRef.setValue(temperatura);
+        }
+}
+}
